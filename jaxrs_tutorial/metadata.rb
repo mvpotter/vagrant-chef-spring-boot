d977b880436d6a39d18f95407b51c081493600f7
Mikhail Potter <potter@azoft.com>
2015-10-09 19:32:59 +0600

    Remove unused folder

--- jaxrs_tutorial/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'jaxrs_tutorial'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures jaxrs_tutorial'
-long_description 'Installs/Configures jaxrs_tutorial'
-
-version          '0.1.0'
-
--- jaxrs_tutorial/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-#
-# Cookbook Name:: jaxrs_tutorial
-# Recipe:: default
-#
-# Copyright (C) 2015 YOUR_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
